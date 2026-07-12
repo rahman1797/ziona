3396b9c5a0478140aaf4684b71449fbd5ddf5236
FA-0713 <maulanarahmannur67@gmail.com>
2026-07-12 10:38:34 +0700

    update copy

--- src/components/sections/featured-collection.tsx
+++ src/components/sections/featured-collection.tsx
@@ -8,7 +8,7 @@ export function FeaturedCollection({ products }: { products: Product[] }) {
       <div className="page-center py-10 flex flex-col gap-6">
         <div className="flex flex-col gap-2 text-center">
           <h2 className="text-[#5D5E4D] font-bold uppercase">Koleksi Kami</h2>
-          <p className="text-[#5D5E4D]">Lorem ipsum dolor sit amet consectetur adipisicing</p>
+          <p className="text-[#5D5E4D]">Temukan model yang sesuai dengan kebutuhanmu</p>
         </div>
         
         <Reveal>
